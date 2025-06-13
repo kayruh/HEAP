1c555bd2a695b684b5d94b5d83d7e0b2f82e0e46
adrian <adrianallinone@gmail.com>
2025-06-13 12:25:09 +0800

    tabs and directory

---
+++ frontend/app/(tabs)/businessProfile.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const businessProfile = () => {
+  return (
+    <View>
+      <Text>businessProfile</Text>
+    </View>
+  )
+}
+
+export default businessProfile
--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -3,5 +3,9 @@ import "./global.css";
 
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+    <Stack.Screen 
+    name="(tabs)" 
+    options={{headerShown:false}}/>
+  </Stack>
 }
