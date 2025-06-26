8636ddcb8e4a874c002bf0d9dbbfaed12f710926
Kayruh <keira.k.2024@computing.smu.edu.sg>
2025-06-26 23:03:53 +0800

    temp carousel

--- frontend/app/(tabs)/browse.tsx
+++ frontend/app/(tabs)/browse.tsx
@@ -1,16 +1,65 @@
 import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
+import { Dimensions } from "react-native";
+import { useSharedValue } from "react-native-reanimated";
+import Carousel, {
+  ICarouselInstance,
+  Pagination,
+} from "react-native-reanimated-carousel";
+
+const data = [...new Array(6).keys()];
+const width = Dimensions.get("window").width;
 
 const browse = () => {
+  const ref = React.useRef<ICarouselInstance>(null);
+  const progress = useSharedValue<number>(0);
+  
+  const onPressPagination = (index: number) => {
+    ref.current?.scrollTo({
+      /**
+       * Calculate the difference between the current index and the target index
+       * to ensure that the carousel scrolls to the nearest index
+       */
+      count: index - progress.value,
+      animated: true,
+    });
+  };
   return (
     <SafeAreaView>
       <Text className='text-2xl font-bold pl-3'>What's Happening</Text>
 
       <Text className='text-sm pl-3'>Description of this tab</Text>
 
-
+      <View style={{ flex: 1 }}>
+        <Carousel
+          ref={ref}
+          width={width}
+          height={width / 2}
+          data={data}
+          onProgressChange={progress}
+          renderItem={({ index }) => (
+            <View
+              style={{
+                flex: 1,
+                borderWidth: 1,
+                justifyContent: "center",
+              }}
+            >
+              <Text style={{ textAlign: "center", fontSize: 30 }}>{index}</Text>
+            </View>
+          )}
+        />
+  
+        <Pagination.Basic
+          progress={progress}
+          data={data}
+          dotStyle={{ backgroundColor: "rgba(0,0,0,0.2)", borderRadius: 50 }}
+          containerStyle={{ gap: 5, marginTop: 10 }}
+          onPress={onPressPagination}
+        />
+      </View>
     </SafeAreaView>
-  )
+  );
 }
 
 export default browse
